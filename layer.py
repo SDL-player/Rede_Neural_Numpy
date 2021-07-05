959804f08ce0f41a75a68c7dac8fc7b6f6d090ad
SDL-player <83915344+SDL-player@users.noreply.github.com>
2021-07-05 16:02:23 -0300

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,4 +1,4 @@
-import numpy as np
+from functions import functions_, np
 
 
 random = np.random
@@ -6,21 +6,21 @@ random = np.random
 
 class Layer:
     def __init__(self, input_, output, function, i=0.1):
+        self.input = input_
+        self.output = output
+        self.name_of_function = function
         self.weights = random.rand(output, input_) * i
-        self.biases = random.rand(output) * i
+        self.biases = np.array([random.rand()] * output)
         self.temp_input = []
-        self.temp_output = []
-        self.f = function
+        self.f = functions_[function]
         
     def __call__(self, x):
         if not isinstance(x, np.ndarray):
             x = np.array(x)
             
         self.temp_input = x
-        x = np.dot(self.weights, x)
+        x = np.dot(self.weights, x) + self.biases
         f = np.vectorize(self.f)
         x = f(x)
-        self.temp_output = x
-        
+
         return x
-    
