51bd401a1c67a05cbfcdd3ffa287047263657483
SDL-player <83915344+SDL-player@users.noreply.github.com>
2021-07-06 22:36:19 -0300

    Update layer.py

--- layer.py
+++ layer.py
@@ -3,6 +3,22 @@ from functions import functions_, np
 
 random = np.random
 
+class Input:
+    def __init__(self, input_, output, i=0.1):
+        self.input = input_
+        self.output = output
+        self.name_of_function = function
+        self.weights = random.rand(output, input_) * i
+        self.biases = random.rand(output) * i
+        
+    def __call__(self, x):
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        self.temp_input = x
+        x = np.dot(self.weights, x) + self.biases
+        
+        return x
 
 class Layer:
     def __init__(self, input_, output, function, i=0.1):
@@ -10,7 +26,7 @@ class Layer:
         self.output = output
         self.name_of_function = function
         self.weights = random.rand(output, input_) * i
-        self.biases = np.array([random.rand()] * output)
+        self.biases = random.rand(output) * i
         self.temp_input = []
         self.f = functions_[function]
         
@@ -19,8 +35,8 @@ class Layer:
             x = np.array(x)
             
         self.temp_input = x
-        x = np.dot(self.weights, x) + self.biases
         f = np.vectorize(self.f)
         x = f(x)
+        x = np.dot(self.weights, x) + self.biases
 
         return x
